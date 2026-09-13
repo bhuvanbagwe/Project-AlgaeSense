78c2541722edda3d8a59b55dc5b7cb5e72ab1542
Bhuvan Bagwe <102174262+bhuvanbagwe@users.noreply.github.com>
2026-09-13 20:51:38 +0530

    Update sketch.ino

--- sketch/sketch.ino
+++ sketch/sketch.ino
@@ -1,48 +1,70 @@
-#include "Arduino_RouterBridge.h"
+#include <Arduino_RouterBridge.h>
 
 const int LIGHT_PIN = D5;
 const int PUMP_PIN = D6;
-const int AERATOR_PIN = D9;
 
-int toPWM(int percent) {
+const int MAX_PUMP_TIME = 10000;
+
+bool pumpRunning = false;
+unsigned long pumpStopTime = 0;
+
+int pwmFromPercent(int percent) {
   percent = constrain(percent, 0, 100);
   return map(percent, 0, 100, 0, 255);
 }
 
-void setLight(int percent) {
-  analogWrite(LIGHT_PIN, toPWM(percent));
+bool setLight(int percent) {
+  analogWrite(LIGHT_PIN, pwmFromPercent(percent));
+  return true;
 }
 
-void setAerator(int percent) {
-  analogWrite(AERATOR_PIN, toPWM(percent));
-}
-
-void samplePump(int durationMs) {
-  durationMs = constrain(durationMs, 100, 10000);
+bool startSample(int durationMs) {
+  durationMs = constrain(durationMs, 100, MAX_PUMP_TIME);
 
   analogWrite(PUMP_PIN, 255);
-  delay(durationMs);
-  analogWrite(PUMP_PIN, 0);
+
+  pumpRunning = true;
+  pumpStopTime = millis() + durationMs;
+
+  return true;
 }
 
-void allOff() {
+bool stopSample() {
+  analogWrite(PUMP_PIN, 0);
+  pumpRunning = false;
+  return true;
+}
+
+bool allOff() {
   analogWrite(LIGHT_PIN, 0);
   analogWrite(PUMP_PIN, 0);
-  analogWrite(AERATOR_PIN, 0);
+  pumpRunning = false;
+  return true;
+}
+
+bool isPumpRunning() {
+  return pumpRunning;
 }
 
 void setup() {
-  analogWriteResolution(8);
-
-  allOff();
-
   Bridge.begin();
+  Monitor.begin(115200);
 
-  Bridge.provide_safe("light", setLight);
-  Bridge.provide_safe("aerator", setAerator);
-  Bridge.provide_safe("sample", samplePump);
+  analogWrite(LIGHT_PIN, 0);
+  analogWrite(PUMP_PIN, 0);
+
+  Bridge.provide_safe("set_light", setLight);
+  Bridge.provide_safe("start_sample", startSample);
+  Bridge.provide_safe("stop_sample", stopSample);
   Bridge.provide_safe("all_off", allOff);
+  Bridge.provide_safe("pump_running", isPumpRunning);
+
+  Monitor.println("AlgaeSense MCU ready");
 }
 
 void loop() {
+  if (pumpRunning &&
+      (long)(millis() - pumpStopTime) >= 0) {
+    stopSample();
+  }
 }
